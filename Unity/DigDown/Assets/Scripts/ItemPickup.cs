097467df3cfa146920e7a2054be395562bb1e618
Marco Minas <marco.minas.246@my.csun.edu>
2024-03-27 18:03:34 -0600

    Added animations, shotgun, need to fix instance variable call

--- Unity/DigDown/Assets/Scripts/ItemPickup.cs
+++ Unity/DigDown/Assets/Scripts/ItemPickup.cs
@@ -2,20 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemPickup : MonoBehaviour
+public class ItemPickup : MonoBehaviour //gameObject in this case would be the player or the pickaxe, but not my shotgun because shotgun object doesnt have this script
 {
+    //-PlayerMovement PM = new PlayerMovement();
+    public GameObject SHOTGUN;
+    PlayerMovement PM; //-forgot what exactly PM is labelled as. Will be used to reference variables from the PlayerMovement script(aka class)
     private Animator anime; //-component variable
+
     // Start is called before the first frame update
     void Start()
     {
-        anime = GetComponent<Animator>(); //-you can use this to call the triggers in the animator
-        
+        UnityEngine.Debug.Log("ItemPickup class called");
+        //anime = GetComponent<Animator>(); //-you can use this to call the triggers in the animator
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Alpha7))//--
+        {
+            Debug.Log("equipped pickaxe");
+            PM.holdShotgun = false;
+            anime.SetBool("HoldShotgun", PM.holdShotgun);
+            //-turn on Jesse's "equip pickaxe" animation
+        }
+        else if (Input.GetKey(KeyCode.Alpha8))
+        {
+            Debug.Log("equipped shotgun");
+            PM.holdShotgun = true;
+            anime.SetBool("HoldShotgun", PM.holdShotgun);
+            //Debug.Log("the current game object is" + gameObject);//- this would just say New Dwarf
+        }//--
     }
 
     private void OnTriggerEnter2D(Collider2D collision) //-check what exactly this function does.
@@ -27,13 +44,14 @@ public class ItemPickup : MonoBehaviour
         }
         if (collision.tag == "Shotgun")
         {
-            anime.SetBool("HoldShotgun", true);
-            Debug.Log("the current game object is" + gameObject);
+            //on collision, remove shotgun from floor, and add to player inventory
+            SHOTGUN = GameObject.Find("Shotgun"); //-SHOTGUN variable now refers to the Shotgun gameObject.
+            Destroy(SHOTGUN);
 
         }
         if (collision.tag == "Weapon")
         {
-            anime.SetBool("HoldShotgun", false);
+            anime.SetBool("HoldShotgun", PM.holdShotgun);
             anime.SetTrigger("Pickaxe");
 
             anime.SetBool("Pickaxe (Hold)", true);
--- Unity/DigDown/Assets/Scripts/PlayerMovement.cs
+++ Unity/DigDown/Assets/Scripts/PlayerMovement.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Timeline;
 
+//This class was called first
 
+//-Problem may be the order of which the scripts are ran. When calling PM.holdShotgun in ItemPickup class,
+//its value may not be what I'm expecting based on PlayerMovement's script iteration.
 public class PlayerMovement : MonoBehaviour //-PlayerMovement is name of the script and class
-
 {
     [SerializeField] private float speed;
     private Rigidbody2D body; //-variable to reference the RigidBody component
     private Animator anime; //-variable to reference the Animator component
     private bool grounded;
+    public bool holdShotgun = false;
+
     // Awake is called at the start
-    private void Awake(){ //- check what exactly this function does
-        body = GetComponent<Rigidbody2D>(); //-check what this means
+    private void Awake() { //- check what exactly this function does
+        body = GetComponent<Rigidbody2D>(); //-check the difference between this line vs a variable of type Rigidbody2D used to change component settings.
         anime = GetComponent<Animator>(); //-check what this means
     }
+
+    void Start()
+    {
+        UnityEngine.Debug.Log("PlayerMovement class called");
+    }
+
     // Update is called when input is recieved.
     //-Update function is called in every frame
     private void Update(){
@@ -27,19 +37,24 @@ public class PlayerMovement : MonoBehaviour //-PlayerMovement is name of the scr
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
         //flips the sprite based on if walking left or right
         if(horizontalInput > 0.01f){
-            transform.localScale = new Vector3(.25f, .25f, .25f);
+            transform.localScale = new Vector3(.25f, .25f, .25f);//- move right
         } else if(horizontalInput < -0.01f){
-            transform.localScale = new Vector3(-.25f, .25f, .25f);
+            transform.localScale = new Vector3(-.25f, .25f, .25f);//-move left
         }
         //Checks for a space bar input and if our player is standing on a ground for a jump action
-        if(Input.GetKey(KeyCode.Space) && grounded){
-            anime.SetBool("HoldShotgun", false);//- my addition
+        if(Input.GetKey(KeyCode.Space) && grounded && holdShotgun){
+            holdShotgun = false; //-plan may be to only hold shotgun while grounded.
+            anime.SetBool("HoldShotgun", holdShotgun);//- my addition
+            Jump();
+        } else if (Input.GetKey(KeyCode.Space) && grounded && holdShotgun == false)
+        {
+            anime.SetBool("HoldShotgun", holdShotgun);
             Jump();
         }
         //starts a running animation
-        anime.SetBool("Run", horizontalInput != 0);
+        anime.SetBool("Run", horizontalInput != 0); //-Run animation automatically plays when horizontalInput is anything but 0. 
         //lets the animations know the player is on the ground
-        anime.SetBool("Grounded", grounded);
+        anime.SetBool("Grounded", grounded); //-Grounded parameter automatically changes. At all times it'll either be grounded or not grounded.
     }
     //handles the Jump command
     private void Jump(){
@@ -55,7 +70,6 @@ public class PlayerMovement : MonoBehaviour //-PlayerMovement is name of the scr
         if(collision.gameObject.tag == "Ground"){
             grounded = true;
         }
-            
     }
     //checks to see if the player is capable of attacking
     //-
